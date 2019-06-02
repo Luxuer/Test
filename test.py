d3e64bc22ef749a4bcba6f031e3c01fe3c563328
Luxuer <470689430@qq.com>
2019-06-02 15:26:15 +0800

    first

---
+++ test.py
@@ -0,0 +1,23 @@
+#! py -2
+# -*- coding: utf-8 -*-
+
+
+# import cv2
+
+# # # 一些点
+# x = [100, 100, 400, 400]
+# y = [200, 500, 200, 500]
+# print x, y
+
+# # 绘制这些点
+# plot(x, y)
+
+import matplotlib.pyplot as plt # plt 用于显示图片
+import matplotlib.image as mpimg # mpimg 用于读取图片
+import numpy as np
+
+print np.random.rand(4, 4)
+
+# input_image = mpimg.imread('session1_center_no_car.png')
+# # print input_image.shape()
+# plt.imshow(input_image)
